16b7fd9184db825bfb35c7f82e30347a60f6c2ba
Satyam Kumar <satyam@Satyams-MacBook-Air.local>
2020-06-14 10:42:56 +0530

    immutable using immer

--- src/index.js
+++ src/index.js
@@ -1,13 +1,1 @@
-import { produce } from "immer";
 
-let book = { title: "My own Glass" };
-
-function adds(book) {
-  book.subTitle = "is not broken";
-  return produce(book, (draftBook) => {
-    draftBook.subtitle2 = "and not empty";
-  });
-}
-let updateSubtilt = adds(book);
-console.log(book);
-console.log(updateSubtilt);
