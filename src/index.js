d555670732f1dcb3c76917f40c55c914824c76b2
Satyam Kumar <satyam@Satyams-MacBook-Air.local>
2020-06-24 08:57:51 +0530

    redux add

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-import store from "./store";
-console.log(store);
-import React from "react";
+import redux from "redux";
+const createStore = redux.createStore;
+
+const BUY_CAKE = "BUY_CAKE";
+
+function buyCake() {
+  return {
+    type: BUY_CAKE,
+    info: "First redux Action",
+  };
+}
+const initialState = {
+  noOfCake: 10,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        noOfCake: state.noOfCake - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+console.log(initialState, store.getState());
